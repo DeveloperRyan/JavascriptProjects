ea3d594e52faf0b7f6b083427e16c74056abb02d
Ryan Elliott <rselliott99@gmail.com>
2020-04-25 16:05:06 -0400

    Add various themes

--- Snake/index.js
+++ Snake/index.js
@@ -5,6 +5,9 @@ const HEIGHT = 500;
 const WIDTH = 500;
 const SCALE = 20;
 const INTERVAL = 100;
+
+let selectedTheme = document.getElementById("theme-select");
+
 const START_POSITION = function () {
 	return {
 		x: SCALE * Math.floor(Math.random() * (WIDTH / SCALE)),
@@ -13,6 +16,10 @@ const START_POSITION = function () {
 };
 
 let directionUpdated = false; // Used to prevent changing direction twice in one game tick.
+
+window.addEventListener("change", (event) => {
+	theme.changeTheme(selectedTheme.value.toLowerCase());
+});
 
 window.addEventListener("keydown", (event) => {
 	const direction = event.key.replace("Arrow", "").toLowerCase();
@@ -26,6 +33,9 @@ window.addEventListener("beforeunload", (event) => {
 function setup() {
 	canvas.height = HEIGHT;
 	canvas.width = WIDTH;
+	theme.changeTheme(selectedTheme.value.toLowerCase());
+	theme.updateTheme();
+
 	food.createFood();
 
 	window.setInterval(() => {
@@ -46,7 +56,7 @@ let snake = {
 	ySpeed: 0,
 	draw: () => {
 		for (cell of snake.cells) {
-			context.fillStyle = "#C2F970";
+			context.fillStyle = theme.selected.snake;
 			context.fillRect(cell.x, cell.y, SCALE, SCALE);
 		}
 	},
@@ -169,7 +179,7 @@ let food = {
 		}
 	},
 	drawFood: () => {
-		context.fillStyle = "#D3FCD5";
+		context.fillStyle = theme.selected.food;
 		context.fillRect(food.position.x, food.position.y, SCALE, SCALE);
 	},
 };
@@ -178,7 +188,7 @@ let score = {
 	highscore: localStorage.getItem("highscore") || 0,
 	score: 0,
 	drawScore: () => {
-		context.fillStyle = "white";
+		context.fillStyle = theme.selected.text;
 		context.textAlign = "right";
 		context.fillText("Score: " + score.score, WIDTH - 15, 10);
 		context.fillText("Highscore: " + score.highscore, WIDTH - 15, 20);
@@ -187,8 +197,83 @@ let score = {
 		score.score = 0;
 	},
 };
+
+const theme = {
+	inchworm: {
+		snake: "#c2f970",
+		background: "#44344f",
+		border: "#564d80",
+		food: "#EDCB96",
+		text: "#ffffff",
+	},
+	icecream: {
+		snake: "#7c3f58",
+		background: "#fff6d3",
+		border: "#ffe5be",
+		food: "#eb6b6f",
+		text: "#000000",
+	},
+	kirokaze: {
+		snake: "#94e344",
+		background: "#332c50",
+		border: "",
+		food: "#46878f",
+		text: "#ffffff",
+	},
+	nyx: {
+		snake: "#816271",
+		background: "#20394f",
+		border: "#0f2a3f",
+		food: "#f6d6bd",
+		text: "#ffffff",
+	},
+	grayscale: {
+		snake: "#000000",
+		background: "#b6b6b6",
+		border: "#676767",
+		food: "#ffffff",
+		text: "#ffffff",
+	},
+	monoramp: {
+		snake: "#8b6d9c",
+		background: "#272744",
+		border: "#494d7e",
+		food: "#fbf5ef",
+		text: "#ffffff",
+	},
+	blessing: {
+		snake: "#96fbc7",
+		background: "#74569b",
+		border: "#ffb3cb",
+		food: "#f7ffae",
+		text: "#ffffff",
+	},
+
+	selected: {
+		snake: "#c2f970",
+		background: "#44344f",
+		border: "#564d80",
+		food: "#d3fcd5",
+		text: "#ffffff",
+	},
+	changeTheme: (newTheme) => {
+		console.log(newTheme);
+		theme.selected.snake = theme[newTheme].snake;
+		theme.selected.background = theme[newTheme].background;
+		theme.selected.border = theme[newTheme].border;
+		theme.selected.food = theme[newTheme].food;
+		theme.selected.text = theme[newTheme].text;
+
+		theme.updateTheme();
+	},
+	updateTheme: () => {
+		canvas.style.backgroundColor = theme.selected.background;
+		canvas.style.borderColor = theme.selected.border;
+	},
+};
 setup();
 
+// For future use
 function getWallDistances() {
 	return [
 		snake.cells[0].y, // North
